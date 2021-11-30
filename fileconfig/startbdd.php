db688aaec08c3f532d18f080ac7e6cc202020b03
ridha-boughediri <ridh.bouhjediri@laplateforme.io>
2021-11-30 11:27:31 +0100

    changement de labase de donnee mysql pour lamettre sur plesk dans sousdomaine

--- fileconfig/startbdd.php
+++ fileconfig/startbdd.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$bdd = new PDO('mysql:host=localhost;dbname=livreor', 'root', '');
+$bdd = new PDO(''mysql:host=localhost:3306 ;dbname=ridha-boughediri_livredor' , 'ridha-boughediri' , '1234fivem@@@');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
